2e6bb2507ba2bcca48d58ad7b6f76a0cdac61a6a
ImMin5 <mino@megazone.com>
2023-12-29 00:21:48 +0900

    build: remove spaceone-core at setup.py and Dockerfile

--- src/setup.py
+++ src/setup.py
@@ -16,23 +16,20 @@
 
 from setuptools import setup, find_packages
 
-with open('VERSION', 'r') as f: 
+with open("VERSION", "r") as f:
     VERSION = f.read().strip()
     f.close()
 
 setup(
-    name='plugin-google-oauth2',
+    name="plugin-google-oauth2",
     version=VERSION,
-    description='Google OAuth2 Plugin',
-    long_description='',
-    url='https://www.spaceone.dev/',
-    author='MEGAZONE SpaceONE Team',
-    author_email='admin@spaceone.dev',
-    license='Apache License 2.0',
+    description="Google OAuth2 Plugin",
+    long_description="",
+    url="https://www.spaceone.dev/",
+    author="MEGAZONE SpaceONE Team",
+    author_email="admin@spaceone.dev",
+    license="Apache License 2.0",
     packages=find_packages(),
-    install_requires=[
-        'spaceone-core',
-        'spaceone-api'
-    ],
+    install_requires=["spaceone-api"],
     zip_safe=False,
 )
